ff1fde1cba7008873076f2c4023cc68a1af44133
viral <viral999g@gmail.com>
2021-12-21 23:44:09 +0530

    Update job files

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -103,6 +103,12 @@ const postEditJob = asyncHandler(async (req, res) => {
 				}
 			);
 
+			const files = updatedJob.files.map((file) => {
+				file.fileURL = 'uploads/files/' + file.fileName;
+				return file;
+			});
+			updatedJob.files = files;
+
 			res.json(updatedJob);
 		} catch (err) {
 			res.status(statusCodes.BAD_REQUEST);
@@ -116,16 +122,21 @@ const postEditJob = asyncHandler(async (req, res) => {
 
 const getJobDetails = asyncHandler(async (req, res) => {
 	const jobID = req.params.jobID;
-	const jobDetails = await jobModel.findById(jobID);
+	const jobDetails = await jobModel.findById(jobID).populate('company');
 
-	if (jobDetails) {
-		const files = jobDetails.files.map((file) => {
+	const jobDetailsJson = jobDetails.toJSON();
+
+	jobDetailsJson.company.imageURL =
+		'/uploads/profilePics/company/' + jobDetailsJson.company.image;
+
+	if (jobDetailsJson) {
+		const files = jobDetailsJson.files.map((file) => {
 			file.fileURL = 'uploads/files/' + file.fileName;
 			return file;
 		});
-		jobDetails.files = files;
+		jobDetailsJson.files = files;
 
-		res.json(jobDetails);
+		res.json(jobDetailsJson);
 	} else {
 		res.status(statusCodes.NOT_FOUND);
 		throw new Error('Job not found!');
