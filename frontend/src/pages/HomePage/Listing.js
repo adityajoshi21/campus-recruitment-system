b6d9830005d64b0b2f12b51b3b45180804ae4ee8
viral <viral999g@gmail.com>
2021-12-22 11:11:45 +0530

    Fixing Icons like a pro

--- frontend/src/pages/HomePage/Listing.js
+++ frontend/src/pages/HomePage/Listing.js
@@ -1,33 +1,59 @@
-import React from "react";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Listing = (props) => {
-
-    return (
-        <div class="job-list">
-        <div class="thumb">
-          <a href="#">
-            <img src={props.image} class="img-fluid" alt="" />
-          </a>
-        </div>
-        <div class="body">
-          <div class="content">
-            <h4><a href="job-details.html">{props.JobTitle}</a></h4>
-            <div class="info">
-              <span class="company"><a href="#"><i data-feather="briefcase"></i>{props.company}</a></span>
-              <span class="office-location"><a href="#"><i data-feather="map-pin"></i>{props.city}</a></span>
-              <span class="job-type full-time"><a href="#"><i data-feather="clock"></i>{props.type}</a></span>
-            </div>
-          </div>
-          <div class="more">
-            <div class="buttons">
-              <a href="#" class="button" data-toggle="modal" data-target="#apply-popup-id">Apply Now</a>
-              <a href="#" class="favourite"><i data-feather="heart"></i></a>
-            </div>
-            <p class="deadline">{props.deadline}</p>
-          </div>
-        </div>
-      </div>
-    );
+	return (
+		<div class="job-list">
+			<div class="thumb">
+				<a href="#">
+					<img src={props.image} class="img-fluid" alt="" />
+				</a>
+			</div>
+			<div class="body">
+				<div class="content">
+					<h4>
+						<a href="job-details.html">{props.JobTitle}</a>
+					</h4>
+					<div class="info">
+						<span class="company">
+							<a href="#">
+								<FontAwesomeIcon icon={faSearch} />
+								{props.company}
+							</a>
+						</span>
+						<span class="office-location">
+							<a href="#">
+								<i data-feather="map-pin"></i>
+								{props.city}
+							</a>
+						</span>
+						<span class="job-type full-time">
+							<a href="#">
+								<i data-feather="clock"></i>
+								{props.type}
+							</a>
+						</span>
+					</div>
+				</div>
+				<div class="more">
+					<div class="buttons">
+						<a
+							href="#"
+							class="button"
+							data-toggle="modal"
+							data-target="#apply-popup-id">
+							Apply Now
+						</a>
+						<a href="#" class="favourite">
+							<i data-feather="heart"></i>
+						</a>
+					</div>
+					<p class="deadline">{props.deadline}</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Listing;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,27 +1,27 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-import { userLoginReducer,userRegisterReducer } from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
 
-const reducer=combineReducers({
-  userLogin: userLoginReducer,
-    userRegister: userRegisterReducer,
-})
+const reducer = combineReducers({
+	userLogin: userLoginReducer,
+	userRegister: userRegisterReducer,
+});
 
 const userInfoFromStorage = localStorage.getItem('userInfo')
-  ? JSON.parse(localStorage.getItem('userInfo'))
-  : null
+	? JSON.parse(localStorage.getItem('userInfo'))
+	: null;
 
-const initialState={
-    userLogin: { userInfo: userInfoFromStorage },
-}
+const initialState = {
+	userLogin: { userInfo: userInfoFromStorage },
+};
 
-const middleware = [thunk]
+const middleware = [thunk];
 
 const store = createStore(
-  reducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-)
-export default store
+	reducer,
+	initialState,
+	composeWithDevTools(applyMiddleware(...middleware))
+);
+export default store;
